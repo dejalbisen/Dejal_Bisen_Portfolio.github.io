9a5e6ddf15e15d7e147940cddd65e33035f0c23d
dejalbisen <dejalbisen06@gmail.com>
2022-05-24 15:39:02 +0530

    Portfolio
    our portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing 
+var typed=new Typed(".typing", {
+    strings:["Front end developer","Web Developer", "Web Designer", "React Developer", "Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
